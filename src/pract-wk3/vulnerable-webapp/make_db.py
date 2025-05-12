6d6936ee8142c0a74b9fb41339c67e873ec59e96
mp32123 <bm.pruis@st.hanze.nl>
2025-05-12 15:47:46 +0200

    Update make_db.py

--- src/pract-wk3/vulnerable-webapp/make_db.py
+++ src/pract-wk3/vulnerable-webapp/make_db.py
@@ -1,9 +1,11 @@
 from electionapp import db, app
 from electionapp.voting.models import Party
 from electionapp.admin.models import User
+from werkzeug.security import generate_password_hash
 
 with app.app_context():
     db.create_all()
     db.session.add_all([Party('Piratenpartij'), Party('DOS 66'), Party('Volkspartij voor Vrijheid van Data')])
-    db.session.add_all([User('admin', 'admin', 'rechts'), User('henk', '1234', 'links')])
+    db.session.add_all([User('admin', generate_password_hash('admin'), 'rechts'), User('henk', generate_password_hash('1234'), 'links')])
+
     db.session.commit()
